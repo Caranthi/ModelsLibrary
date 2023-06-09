1eb3869f45303c8235a47500affee8ece92849d6
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2023-06-09 12:03:39 +0200

    Model printed out

--- SimpleSpring/src/main/java/com/simplespring/SimpleSpring/SimpleSpringApplication.java
+++ SimpleSpring/src/main/java/com/simplespring/SimpleSpring/SimpleSpringApplication.java
@@ -1,9 +1,14 @@
 package com.simplespring.SimpleSpring;
 
+import com.simplespring.SimpleSpring.models.Model;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -15,6 +20,6 @@ public class SimpleSpringApplication {
 	@GetMapping("/") // main content
 	public String hello()
 	{
-		return "hello world";
+		return List.of(new Model("Bowhead Whale", "Black", LocalDate.of(2023, Month.JUNE, 1), 372)).toString();
 	}
 }
--- SimpleSpring/src/main/java/com/simplespring/SimpleSpring/models/Model.java
+++ SimpleSpring/src/main/java/com/simplespring/SimpleSpring/models/Model.java
@@ -1,6 +1,27 @@
 package com.simplespring.SimpleSpring.models;
 
+import java.time.LocalDate;
+
 public class Model {
+    private String species;
+    private String colour;
+    private LocalDate firstAppearance;
+    private int weight;
 
+    public Model(String species, String colour, LocalDate firstAppearance, int weight) {
+        this.species = species;
+        this.colour = colour;
+        this.firstAppearance = firstAppearance;
+        this.weight = weight;
+    }
 
+    @Override
+    public String toString() {
+        return "Model{" +
+                "species='" + species + '\'' +
+                ", colour='" + colour + '\'' +
+                ", firstAppearance=" + firstAppearance +
+                ", weight=" + weight +
+                '}';
+    }
 }
