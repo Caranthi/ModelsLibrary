8837c987543483f23e48e2ce3a6e94199f542077
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2024-02-09 11:20:21 +0100

    Added working Postgres

--- SimpleSpring/src/main/java/com/simplespring/SimpleSpring/repo/Repository.java
+++ SimpleSpring/src/main/java/com/simplespring/SimpleSpring/repo/Repository.java
@@ -1,6 +1,6 @@
 package com.simplespring.SimpleSpring.repo;
 
-import com.simplespring.SimpleSpring.models.Model;
+import com.simplespring.SimpleSpring.model.Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface Repository extends JpaRepository<Model, Integer> { // JpaRepository gives access to API methods
