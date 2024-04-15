437af754b35124da10526eebf7f3094356aa95d5
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2024-04-15 14:11:25 +0200

    Rename project

--- SimpleSpring/src/main/java/com/simplespring/SimpleSpring/repo/Repository.java
+++
@@ -1,7 +0,0 @@
-package com.simplespring.SimpleSpring.repo;
-
-import com.simplespring.SimpleSpring.model.Model;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Repository extends JpaRepository<Model, Integer> { // JpaRepository gives access to API methods
-}
